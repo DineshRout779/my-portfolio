38c62d472ab41181816ffeb075781ecef93709f2
DineshRout779 <rddinesh779@gmail.com>
2021-05-15 12:56:32 +0530

    close nav after click fix

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector(".menu");
 const nav = document.querySelector("nav");
+const links = document.querySelectorAll(".link");
 
-menu.addEventListener("click", function (e) {
-  e.preventDefault();
+menu.addEventListener("click", () => {
   nav.classList.toggle("open");
 });
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", () => {
+    if (nav.classList.contains("open")) {
+      nav.classList.remove("open");
+    }
+  });
+}
