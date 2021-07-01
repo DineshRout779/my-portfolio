a43b42a124bfb5f3fb8f36121366e7c37908b09d
DineshRout779 <rddinesh779@gmail.com>
2021-07-01 13:43:05 +0530

    added projects data

---
+++ data.js
@@ -0,0 +1,23 @@
+const projects = [
+  {
+    image: "./images/BlogApp.png",
+    name: "BlogApp",
+    tech: ["Handlebars", "Node.js", "Express", "MySQL"],
+    live: "https://agile-eyrie-30726.herokuapp.com/",
+    github: "https://github.com/DineshRout779/BlogApp",
+  },
+  {
+    image: "./images/weather-app.png",
+    name: "Weather App",
+    tech: ["React", "OpenWeatherMapAPI"],
+    live: "https://react-weather-app-fawn.vercel.app/",
+    github: "https://github.com/DineshRout779/React-weather-app",
+  },
+  {
+    image: "./images/dash.png",
+    name: "Dashboard",
+    tech: ["HTML", "CSS", "JavScript", "GoogleChartAPI"],
+    live: "https://github.com/DineshRout779/Dashbord-UI",
+    github: "https://dashbord-ui.vercel.app/",
+  },
+];
