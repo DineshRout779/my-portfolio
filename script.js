c2a447bf29b757b2c898c5e021f65035a32e2f7e
Dinesh Rout <rddinesh779@gmail.com>
2021-07-22 20:28:15 +0530

    feat - fetched the projects dynamically from json data

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById("menu");
 const nav = document.querySelector(".nav");
 const nav_links = document.querySelectorAll(".nav_link");
+const levelZeroProjects = document.querySelector(".levelZero");
+const personalProjects = document.querySelector(".personal");
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("open");
@@ -38,4 +40,58 @@ const switchTab = (i) => {
   tabIndicator.style.left = `${(100 / tabs.length) * i}%`;
 };
 
-window.addEventListener("load", switchTab(0));
+window.addEventListener("load", () => {
+  switchTab(0);
+  fetchData();
+});
+
+const allPersonalCards = document.createElement("div");
+
+const fetchData = () => {
+  const url = "./data.json";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      createCards(data);
+    })
+    .catch((e) => console.log(e));
+};
+
+const createCards = (data) => {
+  const { personal } = data[0];
+  // console.log(personal);
+
+  for (let i = 0; i < personal.length; i++) {
+    const card = document.createElement("div");
+    const card_img = document.createElement("div");
+    const img = document.createElement("img");
+    const card_desc = document.createElement("div");
+    const card_tech_list = document.createElement("ul");
+    const card_heading = document.createElement("h4");
+
+    img.src = personal[i].image;
+    img.alt = personal[i].name;
+    card_heading.textContent = personal[i].name;
+
+    const techs = personal[i].techs;
+
+    techs.map((tech) => {
+      const list_item = document.createElement("li");
+      list_item.textContent = tech;
+      card_tech_list.appendChild(list_item);
+    });
+
+    card.classList.add("card");
+    card_img.classList.add("card_img");
+    card_desc.classList.add("card_desc");
+
+    card_img.appendChild(img);
+    card_desc.appendChild(card_tech_list);
+    card_desc.appendChild(card_heading);
+    card.appendChild(card_img);
+    card.appendChild(card_desc);
+    personalProjects.appendChild(card);
+  }
+  console.log(personalProjects);
+};
