9b57533f0e0799820a03bc37cfe937fc1c13be0c
Dinesh Rout <rddinesh779@gmail.com>
2021-07-29 14:53:16 +0530

    refactor - added dynamic html content using template string

--- script.js
+++ script.js
@@ -60,54 +60,36 @@ const fetchData = () => {
 };
 
 const createPersonalCards = (data, len) => {
-  const { personal } = data[0];
+  const {personal}  = data[0];
   // console.log(personal);
 
   for (let i = 0; i < len; i++) {
-    const card = document.createElement("div");
-    const card_img = document.createElement("div");
-    const img = document.createElement("img");
-    const card_desc = document.createElement("div");
-    const card_tech_list = document.createElement("ul");
-    const card_heading = document.createElement("h4");
-    const cta_container = document.createElement("div");
-    const live_link = document.createElement("a");
-    const github_link = document.createElement("a");
-
-    img.src = personal[i].image;
-    img.alt = personal[i].name;
-    card_heading.textContent = personal[i].name;
-    live_link.textContent = "Live";
-    live_link.href = personal[i].live;
-    live_link.target = "_blank";
-    github_link.innerHTML = "<i class='fa fa-github'></i> Code";
-    github_link.href = personal[i].github;
-    github_link.target = "_blank";
-
     const techs = personal[i].techs;
 
-    techs.map((tech) => {
-      const list_item = document.createElement("li");
-      list_item.textContent = tech;
-      card_tech_list.appendChild(list_item);
-    });
+    const tech_list = `<ul>
+      ${techs.map(tech => 
+        `<li>${tech}</li>`
+      ).join(" ")}
+    </ul>`
+    ;
 
-    card.classList.add("card");
-    card_img.classList.add("card_img");
-    card_desc.classList.add("card_desc");
-    cta_container.classList.add("cta_container");
-    live_link.classList.add("cta");
-    github_link.classList.add("cta", "cta_rev");
+    const card = `
+      <div class="card">
+        <div class="card_img">
+          <img src="${personal[i].image}" alt=${personal[i].name}>
+        </div>
+        <div class="card_desc">
+          <div>${tech_list}</div>
+          <h1>${personal[i].name}</h1>
+          <div class="cta_container">
+            <a href="${personal[i].live}" target="_blank" class="cta">Live</a>
+            <a href="${personal[i].github}" target="_blank" class="cta cta_rev"><i class='fa fa-github'></i> Code</a>
+          </div>
+        </div>
+      </div> 
+    `;
 
-    card_img.appendChild(img);
-    card_desc.appendChild(card_tech_list);
-    card_desc.appendChild(card_heading);
-    cta_container.appendChild(live_link);
-    cta_container.appendChild(github_link);
-    card_desc.appendChild(cta_container);
-    card.appendChild(card_img);
-    card.appendChild(card_desc);
-    personalProjects.appendChild(card);
+    personalProjects.innerHTML += card;
   }
 };
 
@@ -119,49 +101,33 @@ const createLevelZeroCards = (data, len) => {
   }
 
   for (let i = 0; i < len; i++) {
-    const card = document.createElement("div");
-    const card_img = document.createElement("div");
-    const img = document.createElement("img");
-    const card_desc = document.createElement("div");
-    const card_tech_list = document.createElement("ul");
-    const card_heading = document.createElement("h4");
-    const cta_container = document.createElement("div");
-    const live_link = document.createElement("a");
-    const github_link = document.createElement("a");
-
-    img.src = levelZero[i].image;
-    img.alt = levelZero[i].name;
-    card_heading.textContent = levelZero[i].name;
-    live_link.textContent = "Live";
-    live_link.href = levelZero[i].live;
-    live_link.target = "_blank";
-    github_link.innerHTML = "<i class='fa fa-github'></i> Code";
-    github_link.href = levelZero[i].github;
-    github_link.target = "_blank";
 
     const techs = levelZero[i].techs;
 
-    techs.map((tech) => {
-      const list_item = document.createElement("li");
-      list_item.textContent = tech;
-      card_tech_list.appendChild(list_item);
-    });
+    const tech_list = `<ul>
+      ${techs.map(tech => 
+        `<li>${tech}</li>`
+      ).join(" ")}
+    </ul>`
+    ;
 
-    card.classList.add("card");
-    card_img.classList.add("card_img");
-    card_desc.classList.add("card_desc");
-    cta_container.classList.add("cta_container");
-    live_link.classList.add("cta");
-    github_link.classList.add("cta", "cta_rev");
+    const card = `
+      <div class="card">
+        <div class="card_img">
+          <img src="${levelZero[i].image}" alt=${levelZero[i].name}>
+        </div>
+        <div class="card_desc">
+          <div>${tech_list}</div>
+          <h1>${levelZero[i].name}</h1>
+          <div class="cta_container">
+            <a href="${levelZero[i].live}" target="_blank" class="cta">Live</a>
+            <a href="${levelZero[i].github}" target="_blank" class="cta cta_rev"><i class='fa fa-github'></i> Code</a>
+          </div>
+        </div>
+      </div> 
+    `;
 
-    card_img.appendChild(img);
-    card_desc.appendChild(card_tech_list);
-    card_desc.appendChild(card_heading);
-    cta_container.appendChild(live_link);
-    cta_container.appendChild(github_link);
-    card_desc.appendChild(cta_container);
-    card.appendChild(card_img);
-    card.appendChild(card_desc);
-    levelZeroProjects.appendChild(card);
+    levelZeroProjects.innerHTML += card;
   }
 };
+
