5b5c8bf9baf39656dd063b8f47498efe376533c7
Dinesh Rout <rddinesh779@gmail.com>
2022-10-05 20:16:19 +0530

    init: initial setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import { AppContextProvider } from './context/AppContext';
+import Projects from './pages/Projects';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'projects', element: <Projects /> },
+      { path: 'blogs', element: <Blogs /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <RouterProvider router={router} />
+    </AppContextProvider>
   </React.StrictMode>
 );
