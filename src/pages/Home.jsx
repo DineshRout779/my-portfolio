8f039b67f0415606537debf339400949c4639c19
Dinesh Rout <rddinesh779@gmail.com>
2022-10-23 17:50:55 +0530

    reafctor: added projects, blogs & spotify status

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
+import BlogList from '../components/BlogList';
 import Experience from '../components/Experience';
 import Intro from '../components/Intro';
+import ProjectList from '../components/ProjectList';
+import SpotifyStatus from '../components/SpotifyStatus';
+import { data } from '../data';
 
 const Home = () => {
+  const { projects } = data;
+
   return (
     <div>
       <Intro />
       <Experience />
+      <ProjectList projects={projects.slice(0, 3)} />
+      <BlogList />
+      <SpotifyStatus />
     </div>
   );
 };
