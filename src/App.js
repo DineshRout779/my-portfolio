e5a127603aeb6646c6de580adbda159c95d37568
Dinesh Rout <rddinesh779@gmail.com>
2022-10-23 17:47:08 +0530

    feat: scroll to top on page change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
+  const path = useLocation();
+
+  // scroll to top on change of route
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [path]);
+
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
