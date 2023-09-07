6418e12c08039105ba34b1717a5867e479c2bd47
Dinesh Rout <rddinesh779@gmail.com>
2023-09-08 02:12:52 +0530

    style: contact page UI updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,63 +1,143 @@
+import { motion } from 'framer-motion';
+
 import styled from 'styled-components';
-import { Container, SectionTitle } from '../styles/globalStyles';
-import { SiGmail } from 'react-icons/si';
-import { AiFillLinkedin } from 'react-icons/ai';
-import { FaTwitter, FaGithub } from 'react-icons/fa';
+import { Container } from '../styles/globalStyles';
+
+const contactIcons = [
+  {
+    url: './icons/gmail.svg',
+    linkTo: 'mailto:rddinesh779@example.co',
+    altText: 'gmail',
+  },
+  {
+    url: './icons/linkedin.svg',
+    linkTo: 'https://www.linkedin.com/in/dineshrout7/',
+    altText: 'linkedin',
+  },
+  {
+    url: './icons/twitter.svg',
+    linkTo: 'https://twitter.com/DineshRout779',
+    altText: 'twitter',
+  },
+];
+
+const container = {
+  hidden: { opacity: 0, y: 300 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+  transition: 0.5,
+};
+
+const item = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Contact = () => {
   return (
     <Container>
-      <Section>
-        <SectionTitle>Contact</SectionTitle>
-        <p>Want to discuss about any project? or just say Hi👋</p>
+      <Wrapper
+        initial={{ opacity: 0, y: 300 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <TextWrapper>
+          <Heading>Contact</Heading>
 
-        <ContactsList>
-          <a href='mailto:rddinesh779@example.com'>
-            <SiGmail />
-            Gmail
-          </a>
-          <a
-            href='https://www.linkedin.com/in/dineshrout7/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <AiFillLinkedin /> Linkedin
-          </a>
-          <a href='https://twitter.com/DineshRout779' rel='noreferrer'>
-            <FaTwitter /> Twitter
-          </a>
-          <a href='https://github.com/DineshRout779' rel='noreferrer'>
-            <FaGithub /> Github
-          </a>
-        </ContactsList>
-      </Section>
+          <Text>
+            I'm currently looking for frontend internships at startups. Have an
+            opening? Please reach out to me.
+          </Text>
+
+          <IconsWrapper variants={container} initial='hidden' animate='visible'>
+            {contactIcons.map((icon, i) => (
+              <IconLink href={icon.linkTo} key={i} variants={item}>
+                <img src={icon.url} alt={icon.altText} />
+              </IconLink>
+            ))}
+          </IconsWrapper>
+        </TextWrapper>
+        <ImageWrapper>
+          <Image src='./imgs/contact.svg' alt='' />
+        </ImageWrapper>
+      </Wrapper>
     </Container>
   );
 };
 export default Contact;
 
-const Section = styled.section`
-  min-height: 90vh;
+const Wrapper = styled(motion.div)`
+  min-height: 70vh;
+  display: flex;
+  justify-content: space-between;
+  gap: 4em;
+  padding: 2em 0;
 
-  p {
-    color: var(--para-gray-color);
+  @media (max-width: 768px) {
+    display: block;
+    text-align: center;
   }
 `;
 
-const ContactsList = styled.div`
-  margin: 1em 0;
-  a {
-    margin: 0.5em 0;
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    padding: 0.5em 1em;
-    border: 1px solid var(--para-gray-color);
-    border-radius: 4px;
-    text-decoration: none;
-
-    svg {
-      font-size: 1.2em;
-    }
+const TextWrapper = styled.div`
+  padding: 2em 0;
+  flex: 0.5;
+  img {
+    display: block;
+    width: 48px;
+    height: 48px;
   }
+`;
+
+const IconsWrapper = styled(motion.div)`
+  display: flex;
+  gap: 1em;
+  margin: 1em 0;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+  }
+`;
+
+const IconLink = styled(motion.a)`
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.3);
+  }
+`;
+
+const Heading = styled.h1`
+  font-size: 2em;
+  line-height: 1.4;
+  margin-bottom: 0.6em;
+`;
+
+const Text = styled.p`
+  @media (max-width: 768px) {
+    width: 80%;
+    margin: 0 auto;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  flex: 0.5;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  display: block;
+  max-width: 480px;
+  margin: 0 auto;
 `;
